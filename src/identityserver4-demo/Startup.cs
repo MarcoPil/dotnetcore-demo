e2e416e245aebeb2587b44935ecca74abd03bd1c
Manuel Riezebosch <mriezebosch@gmail.com>
2016-12-19 14:28:45 +0100

    IdentityServer4 demo in-memory

---
+++ src/identityserver4-demo/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using IdentityServer4.Models;
+using IdentityServer4.Services.InMemory;
+using identityserver4_demo.DummyData;
+
+namespace identityserver4_demo
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddInMemoryClients(Clients.Get())
+                .AddInMemoryIdentityResources(DummyData.Resources.GetIdentityResources())
+                .AddInMemoryApiResources(DummyData.Resources.GetApiResources())
+                .AddInMemoryUsers(Users.Get())
+                .AddTemporarySigningCredential();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
+            app.UseIdentityServer();
+        }
+    }
+}
